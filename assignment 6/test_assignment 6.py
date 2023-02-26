dbe82cc7efa53c346c0ca7dbb7e17ff0951557bc
jazmin <97916000+jazmin431451@users.noreply.github.com>
2023-02-26 13:31:10 -0600

    Update test_assignment 6.py

--- assignment 6/test_assignment 6.py
+++ assignment 6/test_assignment 6.py
@@ -22,26 +22,22 @@ def test_get_text_empty_string_returns_none():
     assert assignment_6.get_text() == None
 
 def test_raises_encode_rle_value_error_numeric_value():
-    with pytest.raises(BaseException):
+    with pytest.raises(Exception):
         assignment_6.encode_rle(float("X"))
 
 
 def test_decode_rle_error_RLE_encoded_text():
-    with pytest.raises(BaseException):
-        assignment_6.decode_rle("a6b4c5", "aaaaaabbbbccccc")
+    with pytest.raises(Exception):
+        assignment_6.decode_rle("aaaaaabbbbccccc", "a6b4c5")
 
-
-def test_decode_rle():
-    with pytest.raises(BaseException):
-        assignment_6.decode_rle('a3b4c2', "aaabbbbcc")
-   
-
+def test_assignment_6():
+    assert assignment_6.encode_rle("aaaaabbbbbbcccc") == "a5b6c4"
 
 def test_decode_rle_assertion_error_text():
-    with pytest.raises(BaseException):
+    with pytest.raises(Exception):
         assignment_6.decode_rle("X", 'a3b4c2')
 
 
 def test_get_text_assertion_error_isdigit():
-    with pytest.raises(BaseException):
+    with pytest.raises(Exception):
         assignment_6.get_text('a3b4c2', "X")
