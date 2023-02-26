5d8ba07331a82c0ebbcbf5162aac61b1f3a57b54
Dave Braunschweig <dave@itrain.ms>
2023-02-26 14:37:02 -0600

    Assignment 6 hints

--- assignment 6/test_assignment 6.py
+++ assignment 6/test_assignment 6.py
@@ -3,13 +3,13 @@ import assignment_6
 
 
 def test_get_text_returns_valid_input():
-    input_values = ['']
+    input_values = ['test']
 
     def input(prompt=None):
         return input_values.pop(0)
 
     assignment_6.input = input
-    assert assignment_6.get_text() == None
+    assert assignment_6.get_text() == "test"
 
 
 def test_get_text_empty_string_returns_none():
@@ -21,23 +21,22 @@ def test_get_text_empty_string_returns_none():
     assignment_6.input = input
     assert assignment_6.get_text() == None
 
-def test_raises_encode_rle_value_error_numeric_value():
-    with pytest.raises(Exception):
-        assignment_6.encode_rle(float("X"))
+
+def test_assignment_6_encode_rle_value_is_correct():
+    assert assignment_6.encode_rle("") == ""
+    assert assignment_6.encode_rle("a") == "a"
+    assert assignment_6.encode_rle("aa") == "a2"
+    assert assignment_6.encode_rle("abc") == "abc"
+    assert assignment_6.encode_rle("abbc") == "ab2c"
+    assert assignment_6.encode_rle("aaabcc") == "a3bc2"
+    assert assignment_6.encode_rle("aaaaaaaaaa") == "a10"
 
 
-def test_decode_rle_error_RLE_encoded_text():
-    with pytest.raises(Exception):
-        assignment_6.decode_rle("aaaaaabbbbccccc", "a6b4c5")
-
-def test_assignment_6():
-    assert assignment_6.encode_rle("aaaaabbbbbbcccc") == "a5b6c4"
-
-def test_decode_rle_assertion_error_text():
-    with pytest.raises(Exception):
-        assignment_6.decode_rle("X", 'a3b4c2')
-
-
-def test_get_text_assertion_error_isdigit():
-    with pytest.raises(Exception):
-        assignment_6.get_text('a3b4c2', "X")
+def test_assignment_6_decode_rle_value_is_correct():
+    assert assignment_6.decode_rle("") == ""
+    assert assignment_6.decode_rle("a") == "a"
+    assert assignment_6.decode_rle("a2") == "aa"
+    assert assignment_6.decode_rle("abc") == "abc"
+    assert assignment_6.decode_rle("ab2c") == "abbc"
+    assert assignment_6.decode_rle("a3bc2") == "aaabcc"
+    assert assignment_6.decode_rle("a10") == "aaaaaaaaaa"
