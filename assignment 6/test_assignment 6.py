ce92eb912c3286f6e53b74d7aecfba35bc755a8b
jazmin <97916000+jazmin431451@users.noreply.github.com>
2023-02-27 22:11:41 -0600

    Update test_assignment 6.py

--- assignment 6/test_assignment 6.py
+++ assignment 6/test_assignment 6.py
@@ -31,12 +31,11 @@ def test_assignment_6_encode_rle_value_is_correct():
     assert assignment_6.encode_rle("aaabcc") == "a3bc2"
     assert assignment_6.encode_rle("aaaaaaaaaa") == "a10"
 
-
 def test_assignment_6_decode_rle_value_is_correct():
     assert assignment_6.decode_rle("") == ""
     assert assignment_6.decode_rle("a") == "a"
-    assert assignment_6.decode_rle("a2") == "aa"
+    assert assignment_6.decode_rle("aa") == "aa"
     assert assignment_6.decode_rle("abc") == "abc"
-    assert assignment_6.decode_rle("ab2c") == "abbc"
-    assert assignment_6.decode_rle("a3bc2") == "aaabcc"
-    assert assignment_6.decode_rle("a10") == "aaaaaaaaaa"
+    assert assignment_6.decode_rle("abbc")== "abbc"
+    assert assignment_6.decode_rle("aaabcc") == "aaabcc"
+    assert assignment_6.decode_rle(str("aaaaaaaaaa")) == "aaaaaaaaaa"
