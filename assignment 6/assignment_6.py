e34b8ff66428851c242cefc775cba2ed1646f6ba
jazmin <97916000+jazmin431451@users.noreply.github.com>
2023-02-28 15:38:42 -0600

    Update assignment_6.py

--- assignment 6/assignment_6.py
+++ assignment 6/assignment_6.py
@@ -1,41 +1,26 @@
 """This program encodes and decodes strings using run-length encoding (RLE).
-
 Input:
     Text string
-
 Output:
     Encoded or decoded string
-
 Example:
     Enter a string or press <Enter> to quit:
      AAABCC
     The encoded string is A3BC2.
-
     Enter a string or press <Enter> to quit:
     ...
-
 References:
     https://en.wikipedia.org/wiki/Run-length_encoding
-
 """
 import sys
 
 def get_text():
-    """Gets text string.
-
-    Args:
-        None
-
-    Returns:
-        string: Text string entered or None if no string entered.
-
     """
-    print("Enter a string or press <Enter> to quit:")
-    text = input()
-    if text == "":
-        return None
-    else:
-        return text
+    Prompt the user to enter some text and return it.
+    If the user enters an empty string, return None.
+    """
+    text = input("Enter some text: ")
+    return text if text != '' else None
 
 
 def encode_rle(text):
@@ -48,17 +33,24 @@ def encode_rle(text):
         encoded_text (string): RLE encoded text.
 
     """
-    encoded_text = ""
-    count = 0
-    for i in range(0, len(text)):
-        count += 1
-        if i + 1 == len(text) or text[i] != text[i + 1]:
-            encoded_text += text[i]
-            if count != 1:
-                encoded_text += str(count)
-            count = 0
-    return encoded_text
+    if not text:
+        return ''
 
+    encoded_text = ''
+    current_char = text[0]
+    count = 1
+
+    for i in range(1, len(text)):
+        if text[i] == current_char:
+            count += 1
+        else:
+            encoded_text += current_char + (str(count) if count > 1 else '')
+            current_char = text[i]
+            count = 1
+
+    encoded_text += current_char + (str(count) if count > 1 else '')
+
+    return encoded_text
 
 def decode_rle(text):
     """Run-length decodes text.
@@ -70,12 +62,122 @@ def decode_rle(text):
         decoded_text (string): RLE decoded text.
 
     """
-    decoded_text = ""
-    for i in range(len(text)):
-        if text[i].isdigit():
-            decoded_text += text[i - 1] * int(text[i])
-        elif not text[i].isdigit():
-            decoded_text += text[i]
+    if not text:
+        return ''
+    decoded_text = ''
+    current_char = ''
+    count_str = ''
+    for char in text:
+        if char.isalpha():
+            decoded_text += current_char * (int(count_str) if count_str else 1)
+            current_char"""This program encodes and decodes strings using run-length encoding (RLE).
+Input:
+    Text string
+Output:
+    Encoded or decoded string
+Example:
+    Enter a string or press <Enter> to quit:
+     AAABCC
+    The encoded string is A3BC2.
+    Enter a string or press <Enter> to quit:
+    ...
+References:
+    https://en.wikipedia.org/wiki/Run-length_encoding
+"""
+import sys
+
+def get_text():
+    """
+    Prompt the user to enter some text and return it.
+    If the user enters an empty string, return None.
+    """
+    text = input("Enter some text: ")
+    return text if text != '' else None
+
+
+def encode_rle(text):
+    """Run-length encodes text.
+
+    Args:
+        text (string): text to be encoded.
+
+    Returns:
+        encoded_text (string): RLE encoded text.
+
+    """
+    if not text:
+        return ''
+
+    encoded_text = ''
+    current_char = text[0]
+    count = 1
+
+    for i in range(1, len(text)):
+        if text[i] == current_char:
+            count += 1
+        else:
+            encoded_text += current_char + (str(count) if count > 1 else '')
+            current_char = text[i]
+            count = 1
+
+    encoded_text += current_char + (str(count) if count > 1 else '')
+
+    return encoded_text
+
+def decode_rle(text):
+    """Run-length decodes text.
+
+    Args:
+        text (string): text to be decoded.
+
+    Returns:
+        decoded_text (string): RLE decoded text.
+
+    """
+    if not text:
+        return ''
+    decoded_text = ''
+    current_char = ''
+    count_str = ''
+    for char in text:
+        if char.isalpha():
+            decoded_text += current_char * (int(count_str) if count_str else 1)
+            current_char = char
+            count_str = ''
+        else:
+            count_str += char
+
+    decoded_text += current_char * (int(count_str) if count_str else 1)
+
+    return decoded_text
+
+
+def main():  # pragma: no cover
+    """Runs the main program logic."""
+
+    try:
+        while True:
+            text = get_text()
+            if text == None:
+                break
+            print(f"You entered {encode_rle(text)}.")
+            print("Here is your decoded string:\n")
+            print(decode_rle(text))
+    except:
+        print("Unexpected error.")
+        print("Error:", sys.exc_info()[1])
+        print("File: ", sys.exc_info()[2].tb_frame.f_code.co_filename) 
+        print("Line: ", sys.exc_info()[2].tb_lineno)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main() = char
+            count_str = ''
+        else:
+            count_str += char
+
+    decoded_text += current_char * (int(count_str) if count_str else 1)
+
     return decoded_text
 
 
