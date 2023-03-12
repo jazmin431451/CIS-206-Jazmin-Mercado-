71bdae2458350b4f64338172f90683f5bc2ee4d8
jazmin <97916000+jazmin431451@users.noreply.github.com>
2023-03-12 18:26:51 -0500

    Update test_file.py

--- assignment 7/test_file.py
+++ assignment 7/test_file.py
@@ -4,38 +4,32 @@ import file
 def test_get_password_returns_valid_input():
     input_values = ['rose']
 
-
     def input(prompt=None):
         return input_values.pop(0)
 
-
     file.input = input
     assert file.get_password() == "rose"
-
 
 def test_get_password_ignores_string_value():
     input_value = ['rosemary1033']  
 
     def input(prompt=None):
         return input_value.pop(0)
-
+    
     file.input = input
     assert file.get_password() == "rosemary1033"
-
 
 def test_get_password_empty_string_returns_none():
     input_values = ['Rosemary1033@-@']
 
-
     def input(prompt=None):
         return input_values.pop(0)
 
- 
     file.input = input
     assert file.get_password() == "Rosemary1033@-@"
 
-
-def test__get_password_returns_password():
-    assert file.get_password('semisoft') == "semisoft"
-    assert file.get_password('semisoft+$$') == "semisoft+$$"
-    assert file.get_password('semisoft+$$*-*') == "semisoft+$$*-*"
+def test_calculate_entropy():
+    assert file.calculate_entropy("j") == "weak"
+    assert file.calculate_entropy("isum") == "medium"
+    assert file.calculate_entropy("jelly") == "strong"
+    assert file.calculate_entropy("jelly22fish") == "very strong"
