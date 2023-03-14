e7a66febf83efd990dd18a676549d4e24b688594
jazmin <97916000+jazmin431451@users.noreply.github.com>
2023-03-14 08:02:08 -0500

    Update test_file.py

--- assignment 7/test_file.py
+++ assignment 7/test_file.py
@@ -10,17 +10,6 @@ def test_get_password_returns_valid_input():
     file.input = input
     assert file.get_password() == "rose"
 
-
-# def test_get_password_ignores_string_value():
-#     input_value = ['rosemary1033']  
-
-#     def input(prompt=None):
-#         return input_value.pop(0)
-    
-#     file.input = input
-#     assert file.get_password() == "rosemary1033"
-
-
 def test_get_password_empty_string_returns_none():
     input_values = ['']
 
@@ -30,9 +19,8 @@ def test_get_password_empty_string_returns_none():
     file.input = input
     assert file.get_password() == None
 
-
 def test_calculate_entropy():
     assert file.calculate_entropy("j") == "weak"
-    assert file.calculate_entropy("isum") == "medium"
-    assert file.calculate_entropy("jelly") == "strong"
+    assert file.calculate_entropy("Isum4") == "medium"
+    assert file.calculate_entropy("jelly22") == "strong"
     assert file.calculate_entropy("jelly22fish") == "very strong"
